77efcc1b216e7776f3e74d77811d3ac1315c5604
aieffei <aieffei@aieffeidemacbook-air.local>
2018-11-07 15:45:11 +0800

    Initial Commit

---
+++ CameraApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CameraApp
+//
+//  Created by aieffei on 2018/11/7.
+//  Copyright © 2018年 黄灿辉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ CameraAppTests/CameraAppTests.m
@@ -0,0 +1,37 @@
+//
+//  CameraAppTests.m
+//  CameraAppTests
+//
+//  Created by aieffei on 2018/11/7.
+//  Copyright © 2018年 黄灿辉. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CameraAppTests : XCTestCase
+
+@end
+
+@implementation CameraAppTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
